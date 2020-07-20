678c25bc0c6965de79b1910da700098bfdae9c62
guptaji6489 <guptaji6489@gmail.com>
2020-07-20 10:08:32 +0530

    a custom modules running on port 4500

---
+++ 04/evalute.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//     dt : function()
+//     {
+//         return 300;
+//     }
+// } 
+
+
+exports.myfun = function()
+{
+    return "hellooooo";
+}
+
+
+function sum(a,b)
+{
+    return a+b;
+}
+
+
+var str = "hello i am harshit gupta";
+exports.sum =  sum;
+exports.str = str;
---
+++ 04/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var evalute = require('./evalute');
+
+function onrequest(req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write("hello harshit from nodejs");
+    res.write("\n" + evalute.sum(10,20));
+    res.write("\n"+ evalute.str)
+    res.write("\n"  + evalute.myfun());
+    res.end();
+}
+http.createServer(onrequest).listen(4000);
+
+console.log("server created!!");
+
