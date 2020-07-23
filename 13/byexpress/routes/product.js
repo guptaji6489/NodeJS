aead0b369a0da1c301a693d9be0a319bc6bb6054
guptaji6489 <guptaji6489@gmail.com>
2020-07-23 17:42:22 +0530

    CRUD Application using Nodejs and MYSQL you can create, update, edit, delete your Information which is running on port 4500

--- 13/byexpress/routes/product.js
+++ 13/byexpress/routes/product.js
@@ -1,18 +1,70 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var connection = require('../db');
+var db = require("../db");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    var query = 'select * from student1';
-    connection.query(query,function(err,rows,fields){
-        if(err) throw err;
-        // res.json(rows);
-        res.render('product', { title: 'product',product:rows });
+router.get("/", function (req, res, next) {
+  var query = "select * from student1";
+  db.query(query, function (err, rows, fields) {
+    if (err) throw err;
+    // res.json(rows);
+    res.render("product", { title: "Products", product: rows });
+  });
 });
-    })
-  
 
+router.get("/create-form", function (req, res, next) {
+  res.render("createform", { title: "Create Product" });
+});
 
+router.post("/create", function (req, res, next) {
+  var product_name = req.body.product_name;
+  var sku = req.body.sku;
+  var price = req.body.price;
+  var std = req.body.std;
+
+  var sql = `INSERT INTO student1( rollno, name, city, standard) VALUES ("${product_name}", "${sku}", "${price}", "${std}")`;
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("record inserted");
+    res.redirect("/product");
+  });
+});
+
+router.get("/edit-form/:id", function (req, res, next) {
+  var id = req.params.id;
+  var sql = `SELECT * FROM student1 WHERE id=${id}`;
+  db.query(sql, function (err, rows, fields) {
+    res.render("editform", { title: "Edit Product", product: rows[0] });
+  });
+});
+
+router.post("/edit/:id", function (req, res, next) {
+  var product_name = req.body.product_name;
+  var sku = req.body.sku;
+  var price = req.body.price;
+  var id = req.params.id;
+  var std = req.body.std;
+
+  var sql = `UPDATE student1 SET rollno="${product_name}", name="${sku}", city="${price}",standard="${std}" WHERE id=${id}`;
+
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("record updated!");
+    res.redirect("/product");
+  });
+});
+
+/*delete product*/
+router.get("/delete/:id", function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  var sql = `DELETE FROM student1 WHERE id=${id}`;
+
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("record deleted!");
+    res.redirect("/product");
+  });
+});
 
 module.exports = router;
